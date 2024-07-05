ae00eb658ed422313c23c43795a35bd095066548
gabwrel <82355336+gabwrel@users.noreply.github.com>
2024-07-05 22:11:02 +0800

    initial commit

---
+++ controllers/authController.js
@@ -0,0 +1,24 @@
+const User = require('../models/User');
+
+async function registerUser(req, res){
+    let {firstName, lastName, username, password} = req.body;
+    try{
+        const duplicate = await User.find({username});
+        if (duplicate && duplicate.length > 0 ){
+            return res.status(400).send({message:'User already registered with this username'}); 
+        }
+        let user = new User({firstName, lastName, username, password});
+        const result = await user.save();
+        console.log(result);
+        res.status(201).send({message: 'User registerd successfully!'});
+    }catch(err){
+        console.log(err); 
+        res.status(400).send(err);
+    }
+}
+
+const AuthController = {
+    registerUser
+}
+
+module.exports = AuthController;
