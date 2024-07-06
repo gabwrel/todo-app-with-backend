e6a772f7adbb9cd60e85b971a0e422e7beb88813
gabwrel <82355336+gabwrel@users.noreply.github.com>
2024-07-06 15:13:04 +0800

    update

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const AuthController = require ('../controllers/authController');
 
 router.post('/register', AuthController.registerUser)
+router.post('/login', AuthController.loginUser)
 
 module.exports = router;
--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-app.use('api', authRoutes)
+app.use('api', authRoutes )
 
 mongoose.connect(process.env.DB_URL).then((result)=>{
     console.log("Successfully Connected to DB!");
