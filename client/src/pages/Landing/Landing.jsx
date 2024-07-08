a07c7c605bcd4b937ab9aeffc83372d23b9f5559
gabwrel <82355336+gabwrel@users.noreply.github.com>
2024-07-09 01:23:54 +0800

    Update Landing.jsx

--- client/src/pages/Landing/Landing.jsx
+++ client/src/pages/Landing/Landing.jsx
@@ -8,10 +8,10 @@ function Landing() {
   return (
     <div>
         <Navbar/>
-        <section className='h-svh w-svw flex flex-row'>
-          <div className='w-1/2 h-full flex flex-col p-4 justify-center'>
-            <h1 className='font-bold text-9xl'>Welcome to JGD's To Do List App</h1>
-            <div className='mt-8'>
+        <section className='h-svh w-svw flex flex-col mt-20 sm:flex-row sm:mt-0 sm:px-20'>
+          <div className='w-full sm:w-1/2 h-full flex flex-col p-4 justify-center'>
+            <h1 className='font-extrabold text-4xl sm:text-7xl'>Welcome to JGD's To Do List App</h1>
+            <div className='mt-12'>
               <Link to="/register" className='text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-lg px-8 py-4 me-8 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800'>
                 Register
               </Link>
@@ -20,7 +20,7 @@ function Landing() {
               </Link>
             </div>
           </div>
-          <div className='w-1/2 h-full p-4'>
+          <div className='w-full sm:w-1/2 h-full flex flex-col p-4 justify-center'>
             <img src={hero} alt="hero" />
           </div>
         </section>
