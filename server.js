867d0787cf678a8cfc9c41e8f9471ecdf3c11d39
gabwrel <82355336+gabwrel@users.noreply.github.com>
2024-07-12 20:20:31 +0800

    Update server.js

--- server.js
+++ server.js
@@ -10,14 +10,12 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-app.use('api', authRoutes )
+app.use('/api', authRoutes );
 
-mongoose.connect(process.env.DB_URL).then((result)=>{
-    console.log("Successfully Connected to DB!");
-}).catch(err=>{
-    console.log(err);
-})
+mongoose.connect(process.env.DB_URL)
+    .then(() => console.log("Successfully Connected to DB!"))
+    .catch(err => console.log(err));
 
 app.listen(PORT, () => {
     console.log(`Server started at port ${PORT}`);
-})
+});
