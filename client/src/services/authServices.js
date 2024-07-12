12144615e995516538e7bd1d7048764d4285bfc5
gabwrel <82355336+gabwrel@users.noreply.github.com>
2024-07-12 21:42:16 +0800

    Update authServices.js

--- client/src/services/authServices.js
+++ client/src/services/authServices.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const SERVER_URL = "https://localhost:5000/api";
+const SERVER_URL = "http://localhost:5000";
 
 const registerUser = (data) => {
     return axios.post(SERVER_URL+'/register', data);
@@ -10,7 +10,7 @@ const loginUser = (data) => {
     return axios.post(SERVER_URL+'/login', data);
 } 
 
-const AuthServices = {
+const AuthServices = {  
     registerUser,
     loginUser,
 }
