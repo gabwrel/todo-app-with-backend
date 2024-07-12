efc7c63b715ce47cabed3c9d0b2445cb8b9812eb
gabwrel <82355336+gabwrel@users.noreply.github.com>
2024-07-12 20:20:39 +0800

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,46 +1,48 @@
 const User = require('../models/User');
-const jwt = require('jsonwebtoken')
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
-const secretKey = process.env.JWTSECRET
+const secretKey = process.env.JWTSECRET;
 
-async function registerUser(req, res){
-    let {firstName, lastName, username, password} = req.body;
-    try{
-        const duplicate = await User.find({username});
-        if (duplicate && duplicate.length > 0 ){
-            return res.status(400).send({message:'User already registered with this username'}); 
+async function registerUser(req, res) {
+    console.log('Handling registerUser');
+    const { firstName, lastName, username, password } = req.body;
+    try {
+        const duplicate = await User.find({ username });
+        if (duplicate && duplicate.length > 0) {
+            return res.status(400).send({ message: 'User already registered with this username' });
         }
-        let user = new User({firstName, lastName, username, password});
+        const user = new User({ firstName, lastName, username, password });
         const result = await user.save();
         console.log(result);
-        res.status(201).send({message: 'User registerd successfully!'});
-    }catch(err){
-        console.log(err); 
+        res.status(201).send({ message: 'User registered successfully!' });
+    } catch (err) {
+        console.log(err);
         res.status(400).send(err);
     }
 }
 
-async function loginUser(req, res){
-    try{
-        const {username, password} = req.body;
-        const user = await User.findOne({username});
+async function loginUser(req, res) {
+    console.log('Handling loginUser');
+    try {
+        const { username, password } = req.body;
+        const user = await User.findOne({ username });
         if (!user) {
-            return res.status(404).send({message:"Authentication Failed"});
+            return res.status(404).send({ message: "Authentication Failed" });
         }
         const isPasswordValid = await user.comparePassword(password);
         if (!isPasswordValid) {
-            return res.status(404).semd({message:"Incorrect Password."});
+            return res.status(404).send({ message: "Incorrect Password." });
         }
-        let token = await jwt.sign({userId:user?._id}, secretKey, {expiredIn: '1h'});
-        let finalData = {
-            userId:user?._id,
-            username:user?.username,
-            firstname:user?.lastname,
+        const token = jwt.sign({ userId: user?._id }, secretKey, { expiresIn: '1h' });
+        const finalData = {
+            userId: user?._id,
+            username: user?.username,
+            firstname: user?.lastname,
             token
-        }
+        };
         res.send(finalData);
-    }catch(err){
+    } catch (err) {
         console.log(err);
         res.status(400).send(err);
     }
@@ -49,6 +51,6 @@ async function loginUser(req, res){
 const AuthController = {
     registerUser,
     loginUser
-}
+};
 
 module.exports = AuthController;
